35fd57e32b8ffcd7f3ff6e0fe76a355af66a9f83
vicky1312 <victoryabayomi@gmail.com>
2023-10-27 16:12:43 +0100

    Installing tailwind

--- agriproject/src/App.js
+++ agriproject/src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
-import Main from './components/main.js'
+import Main from './components/index.js'
 import Animal from './components/animal.js'
 
 function App() {
